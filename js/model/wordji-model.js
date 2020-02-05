092f01200f295f63f7c8e350ba5906c5a525d83d
riccardou <christianr@rocketmail.com>
2020-02-05 12:28:53 -0800

    add variables to keep track of game state

---
+++ js/model/wordji-model.js
@@ -0,0 +1,28 @@
+(function(window) {
+    'use-strict';
+
+    let wordjiModel = {
+        wordBank: {
+            0: 'yet',
+            1: 'another',
+            2: 'test'
+        },
+        guessCounter: '',
+        guessedLetters: [],
+        selectedWord: '',
+        selectRandomWordKey: function() {
+            let randomKey = Math.floor(Math.random() * Object.keys(this.wordBank).length);
+            this.selectedWord = this.wordBank[randomKey];
+            this.removeSelectedWord(randomKey);
+        },
+        removeSelectedWord: function(randomKey) {
+            delete this.wordBank[randomKey];
+        },
+        init: function() {
+            this.selectRandomWordKey();
+        }
+    }
+
+    window.app = window.app || {};
+    window.app.wordjiModel = wordjiModel;
+})(window);
