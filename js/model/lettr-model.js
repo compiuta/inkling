a6f5fbdc1c76f0436afad24b44422fcb5617a1ff
riccardou <christianr@rocketmail.com>
2020-02-08 10:50:48 -0800

    add guessedWords empty array and addGuessToArray function

--- js/model/lettr-model.js
+++ js/model/lettr-model.js
@@ -9,12 +9,21 @@
         },
         guessCounter: 0,
         guessedLetters: [],
+        guessedWords: [],
         selectedWord: '',
         allowedGuesses: 6,
         selectRandomWordKey: function() {
             let randomKey = Math.floor(Math.random() * Object.keys(this.wordBank).length);
             this.selectedWord = this.wordBank[randomKey];
             this.removeSelectedWord(randomKey);
+        },
+        addGuessToArray: function(guess, isGuessWord) {
+            if(isGuessWord) {
+                this.guessedWords.push(guess);
+            } else {
+                this.guessedLetters.push(guess);
+                this.guessedLetters.sort();
+            }
         },
         removeSelectedWord: function(randomKey) {
             delete this.wordBank[randomKey];
