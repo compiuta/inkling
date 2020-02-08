998508de0cd98ea6ff04ca7bce85b18117b6ed77
riccardou <christianr@rocketmail.com>
2020-02-08 10:54:34 -0800

    modified validateGuess functions with radio states and added word guessing functionality

--- js/controller/lettr-controller.js
+++ js/controller/lettr-controller.js
@@ -12,19 +12,29 @@
             return this.wordLetterArray;
         },
         wordFormatToDisplay: function() {
-            this.formatWordToArray();
-            
-            this.wordToDisplay = '';
 
-            for(let i = 0; i < this.wordLetterArray.length; i++) {
-                if(app.lettrModel.guessedLetters.includes(this.wordLetterArray[i])) {
-                    this.wordToDisplay += this.wordLetterArray[i]
-                } else {
-                    this.wordToDisplay += ' _ ';
+            if(app.lettrView.formWordRadioOption.checked) {
+
+                return app.lettrModel.selectedWord;
+
+            } else {
+
+                this.formatWordToArray();
+
+                this.wordToDisplay = '';
+
+                for(let i = 0; i < this.wordLetterArray.length; i++) {
+                    if(app.lettrModel.guessedLetters.includes(this.wordLetterArray[i])) {
+                        this.wordToDisplay += this.wordLetterArray[i]
+                    } else {
+                        this.wordToDisplay += ' _ ';
+                    }
                 }
+
+                return this.wordToDisplay;
+
             }
 
-            return this.wordToDisplay;
         },
         guessesAvailable: function() {
             let guessAvailable ={
@@ -36,9 +46,26 @@
         },
         validateUserGuess: function(e) {
             e.preventDefault();
-    
-            let guess = app.lettrView.userGuessInput.value;
-            let regex=/^[a-zA-Z]+$/;
+
+            let guess = app.lettrView.userGuessInput.value.toLowerCase().replace(/ /g, '');
+
+            if(app.lettrView.formLetterRadioOption.checked) {
+
+                app.lettrController.userGuessedLetter(guess);
+
+            } else if(app.lettrView.formWordRadioOption.checked) {
+
+                app.lettrController.userGuessedWord(guess);
+
+            } else {
+
+                alert('Please select a guess option :)');
+
+            }
+            
+        },
+        userGuessedLetter: function(guess){
+            let regex = /^[a-zA-Z]+$/;
 
             if (guess.match(regex)){
                 app.lettrController.isGuessDuplicate(guess);
@@ -55,8 +82,23 @@
                 this.checkUserGuess(guess);
             }
         },
+        userGuessedWord: function(guess) {
+            app.lettrModel.addGuessToArray(guess, true);
+
+            if(app.lettrModel.selectedWord === guess) {
+                alert('That is the correct word! :)');
+                app.lettrView.displayWord();
+            } else {
+                app.lettrModel.guessCounter++;
+                alert('That is not the correct word :(');
+                app.lettrView.populateAvailableGuesses();
+            }
+
+            app.lettrView.populateGuessedElementContainers(app.lettrModel.guessedWords, app.lettrView.guessWordsContainer);
+
+        },
         checkUserGuess: function(guess) {
-            app.lettrModel.guessedLetters.push(guess);
+            app.lettrModel.addGuessToArray(guess);
 
             if(this.wordLetterArray.includes(guess)) {
                 alert('letter exists in word!');
@@ -67,7 +109,7 @@
                 app.lettrView.populateAvailableGuesses();
             }
 
-            app.lettrView.populateSelectedLetters(app.lettrModel.guessedLetters);
+            app.lettrView.populateGuessedElementContainers(app.lettrModel.guessedLetters, app.lettrView.guessedLettersContainer, true);
         },
         init: function() {
             app.lettrModel.init();
