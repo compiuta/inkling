d830ca626024e0cff582e8e90a4cb66571f5415a
riccardou <christianr@rocketmail.com>
2020-02-09 16:18:38 -0800

    add words array to populate wordBank and add parameter to clearBoardModel

--- js/model/lettr-model.js
+++ js/model/lettr-model.js
@@ -2,17 +2,25 @@
     'use strict';
 
     let lettrModel = {
-        wordBank: [
+        words: [
             'yet',
             'another',
             'test'
         ],
+        wordBank: [],
         incorrectGuessCounter: 0,
         guessedLetters: [],
         guessedWords: [],
         selectedWord: '',
         allowedGuesses: 6,
         userScore: 0,
+        populateWordBank: function() {
+            this.wordBank = [];
+
+            for(let i = 0; i < this.words.length; i++) {
+                this.wordBank.push(this.words[i]);
+            }
+        },
         selectRandomWordKey: function() {
             let randomKey = Math.floor(Math.random() * this.wordBank.length);
             this.selectedWord = this.wordBank[randomKey];
@@ -29,13 +37,20 @@
         removeSelectedWord: function(randomKey) {
             this.wordBank.splice(randomKey, 1);
         },
-        clearBoardModel() {
+        clearBoardModel: function(newGame) {
+
             this.guessedLetters = [];
             this.guessedWords = [];
+
+            if(newGame) {
+                this.incorrectGuessCounter = 0;
+                this.populateWordBank();
+            }
 
             this.selectRandomWordKey();
         },
         init: function() {
+            this.populateWordBank();
             this.selectRandomWordKey();
         }
     }
